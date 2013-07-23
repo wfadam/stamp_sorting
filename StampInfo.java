aac325f8e500b2c94fe1bb913da2bd407efc86d4
wfadam <wfadamweb@gmail.com>
2013-07-23 18:47:21 +0800

    Create StampInfo.java
    Class for stamp info containing

---
+++ StampInfo.java
@@ -0,0 +1,16 @@
+package com.s;
+
+
+public class StampInfo{
+  public String name	= "";
+	public String group	= "";
+	public int startCol	= 0;
+	public int colCnt	= 0;
+	public int page		= 0;
+	public int exp		= 0;
+
+	public StampInfo(){
+
+	}
+}
+
